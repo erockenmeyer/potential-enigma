6409ed6e81c3eb0a3f92b87a576a12fe8471363b
erockenmeyer <erikrockenmeyer@gmail.com>
2022-04-10 22:22:48 -0500

    completed file generation code

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -4,6 +4,7 @@ function renderLicenseBadge(license) {
   if (!license) {
     return '';
   }
+  return ``
 }
 
 // function that returns the license link
@@ -12,6 +13,7 @@ function renderLicenseLink(license) {
   if (!license) {
     return '';
   }
+  return `- [License](README.md#license)`
 }
 
 // function that returns the license section of README
@@ -20,12 +22,43 @@ function renderLicenseSection(license) {
   if (!license) {
     return '';
   }
+  return `
+## License
+  `;
 }
 
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+## Table of Contents
+- [Description](README.md#description)
+- [Installation](README.md#installation)
+- [Usage](README.md#usage)
+${renderLicenseLink(data.license)}
+- [Contributing](README.md#contributing)
+- [Tests](README.md#tests)
+- [Questions](README.md#questions)
 
+## Description
+${data.description}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+${renderLicenseSection(data.license)}
+${renderLicenseBadge(data.license)}
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+Github: [${data.username}](${data.github})
+Email: ${data.email}
+${data.contact}
 `;
 }
 
