f478f46a8445ccb10499ea69f4f518f10ada6278
erockenmeyer <erikrockenmeyer@gmail.com>
2022-04-10 21:50:45 -0500

    questions and validations completed

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,9 @@
-// TODO: Include packages needed for this application
+// packages needed for this application
+const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown')
 
-// TODO: Create an array of questions for user input
+// array of questions for user input
 const questions = [
     {
         type: 'input',
@@ -33,12 +34,28 @@ const questions = [
     {
         type: 'input',
         name: 'installation',
-        message: 'How is your project installed?'
+        message: 'How is your project installed?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please explain how to install your project.');
+                return false;
+            }
+        }
     },
     {
         type: 'input',
         name: 'usage',
-        message: 'How should your project be used?'
+        message: 'How should your project be used?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please explain the use of project.');
+                return false;
+            }
+        }
     },
     {
         type: 'input',
@@ -48,27 +65,67 @@ const questions = [
     {
         type: 'input',
         name: 'contributing',
-        message: 'Who contributed to this project?'
+        message: 'Who contributed to this project?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter the name(s) of the contributors.');
+                return false;
+            }
+        }
     },
     {
         type: 'input',
         name: 'tests',
-        message: 'What tests does this project have?'
+        message: 'What tests does this project have?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please state what tests exist for this project.');
+                return false;
+            }
+        }
     },
     {
         type: 'input',
         name: 'username',
-        message: 'What github username should be contacted for further information?'
+        message: 'What github username should be contacted for further information?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter a github username.');
+                return false;
+            }
+        }
     },
     {
         type: 'input',
         name: 'github',
-        message: 'What is the link for that github?'
+        message: 'What is the link for that github?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please link to the github.');
+                return false;
+            }
+        }
     },
     {
         type: 'input',
         name: 'email',
-        message: 'What email address can people contact for more information?'
+        message: 'What email address can people contact for more information?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please provide an email.');
+                return false;
+            }
+        }
     },
     {
         type: 'input',
@@ -77,10 +134,24 @@ const questions = [
     }
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// write README file
+function writeToFile(fileName, data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(fileName, data, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
 
-// TODO: Create a function to initialize app
+            resolve({
+                ok: true,
+                message: 'README created!'
+            })
+        })
+    })
+}
+
+// function to initialize app
 function init() {
     return inquirer.prompt(questions)
     .then(projectData => {
